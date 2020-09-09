d111928269fe7ee27221a44ef3dd0d698d23730e
Preeti Hans <preeti.kuk91@gmail.com>
2020-09-09 18:44:04 +0530

    adding test coverage

--- es6/__tests__/table.spec.js
+++ es6/__tests__/table.spec.js
@@ -1,34 +1,31 @@
 import Table from "../table";
-//const TableCls = require("../table.js");
+import Data  from "../testData.json";
 describe("test calculateData", () => {
-  test("it should call addData", () => {
-    const addData = Table.prototype.addData = jest.fn();
+  test("it should render table", () => {
+    const testData = { body: JSON.stringify(Data)};
+
+    document.body.innerHTML = "<div id=\"resp-table-body\">\n" +
+      "            <div id=\"resp-table-row\"></div>\n" +
+      "        </div>";
+    window.Sparkline = {draw: jest.fn()};
     const t = new Table();
-    const testData = { body: {"name":"usdjpy","bestBid":106.7297012204255,"bestAsk":107.25199883791178,"openBid":107.22827132623534,"openAsk":109.78172867376465,"lastChangeAsk":-4.862314256927661,"lastChangeBid":-2.8769211401569663}}
+    jest.spyOn(t, 'addData');
+    jest.spyOn(t, 'appendChild');
+    jest.spyOn(t, 'sortSparkLineData');
+    jest.spyOn(t, 'renderData');
+    jest.spyOn(t, 'getData');
     t.addData(testData);
+    expect(t.data).toMatchObject({});
+    expect(t.data).toHaveProperty(Data.name);
+    expect(t.addData).toHaveBeenCalledTimes(1);
+    expect(t.addData).toHaveBeenCalledWith(testData);
+    expect(t.sortSparkLineData).toHaveBeenCalled();
+    expect(t.appendChild).toHaveBeenCalled();
+    expect(t.renderData).toHaveBeenCalled();
+    expect(t.getData).toHaveBeenCalled();
+    const responseHtml = "<div id=\"resp-table-body\"><div class=\"resp-table-row\"><div class=\"table-body-cell\">usdjpy</div><div class=\"table-body-cell\">106.7297012204255</div><div class=\"table-body-cell\">107.25199883791178</div><div class=\"table-body-cell\">-2.8769211401569663</div><div class=\"table-body-cell\">-4.862314256927661</div><div class=\"table-body-cell\"><span></span></div></div></div>";
+
+    expect(document.body.innerHTML).toBe(responseHtml);
   });
-  test("it should call sortSparkLineData", () => {
-    const sortSparkLineData = Table.prototype.sortSparkLineData = jest.fn();
-    const t = new Table();
-    t.sortSparkLineData();
-    expect(sortSparkLineData).toHaveBeenCalledTimes(1);
-  });
-  test("it should call renderData", () => {
-    const renderData = Table.prototype.renderData = jest.fn();
-    const t = new Table();
-    t.renderData();
-    expect(renderData).toHaveBeenCalledTimes(1);
-  });
-  test("it should call getData", () => {
-    const getData = Table.prototype.getData = jest.fn();
-    const t = new Table();
-    t.getData();
-    expect(getData).toHaveBeenCalledTimes(1);
-  });
-  test("it should call getData", () => {
-    const appendChild = Table.prototype.appendChild = jest.fn();
-    const t = new Table();
-    t.appendChild();
-    expect(appendChild).toHaveBeenCalledTimes(1);
-  });
+
 });
